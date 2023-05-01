ae4f53c7078bd8bf1e14b60947a359b301f1b5f6
Malasuerte94 <ene.catalin.94@gmail.com>
2023-05-01 21:29:47 +0300

    Remove unussed

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,11 @@
-import CurvedBottomNavigation from './components/CurvedBottomNavigation.vue';
-import GrowBottomNavigation from './components/GrowBottomNavigation.vue';
-import RingBottomNavigation from './components/RingBottomNavigation.vue';
 import SwipeBottomNavigation from './components/SwipeBottomNavigation.vue';
-import WindowsBottomNavigation from './components/WindowsBottomNavigation.vue';
-import HillBottomNavigation from './components/HillBottomNavigation.vue';
 
 const plugin = {
   install(Vue: any) {
-    Vue.component('CurvedBottomNavigation', CurvedBottomNavigation);
-    Vue.component('GrowBottomNavigation', GrowBottomNavigation);
     Vue.component('SwipeBottomNavigation', SwipeBottomNavigation);
-    Vue.component('RingBottomNavigation', RingBottomNavigation);
-    Vue.component('WindowsBottomNavigation', WindowsBottomNavigation);
-    Vue.component('HillBottomNavigation', HillBottomNavigation);
   },
 };
 
-export {
-  CurvedBottomNavigation,
-  GrowBottomNavigation,
-  RingBottomNavigation,
-  SwipeBottomNavigation,
-  WindowsBottomNavigation,
-  HillBottomNavigation,
-};
+export { SwipeBottomNavigation };
 
 export default plugin;
